0ad10be48a124ec27ed6cdfdb9aa208582bf5996
Denali Balser <denali.balser167@gmail.com>
2019-05-16 11:33:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,11 @@ require "pry"
 def reverse_each_word(string)
   reversed_array = []
   string = string.split(" , ")
-  string.each do |word|
+  string.each do |sentence|
+    sentence.each do |word|
     reversed_array << word.reverse
 binding.pry
 
   end
+  reversed_array.join(" ")
 end
