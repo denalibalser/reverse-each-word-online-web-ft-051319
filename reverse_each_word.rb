f1bb28a1c7ef24a9d2a9cd5088426294a610c9bf
Denali Balser <denali.balser167@gmail.com>
2019-05-16 11:30:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(string)
   reversed_array = []
   string = string.split(" , ")
   string.each do |word|
-    reversed_array << word.reverse 
-    
+    reversed_array << word.reverse
+
 
   end
 end
